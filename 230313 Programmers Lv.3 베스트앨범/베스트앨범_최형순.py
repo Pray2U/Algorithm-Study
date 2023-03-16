c6a279846eba78e12df1c7803af0230a024b2785
HS980924 <75660071+HS98094@users.noreply.github.com>
2023-03-16 09:00:33 +0900

    Update Lv.1 문자열 나누기 & Lv.3 베스트앨범

---
+++ 230313 Programmers Lv.3 베스트앨범/베스트앨범_최형순.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+def solution(genres, plays):
+    answer = []
+    dictSum = defaultdict(int)
+    dictGenres = defaultdict(list)
+    
+    for idx in range(len(genres)):
+        dictSum[genres[idx]] += plays[idx]
+        dictGenres[genres[idx]].append((idx,plays[idx]))
+    
+    for key in sorted(dictSum,key=lambda x:dictSum[x],reverse=True):
+        dictGenres[key].sort(key=lambda x:(x[1],-x[0]))
+        cnt = 0
+        while dictGenres[key] and cnt < 2:
+            value = dictGenres[key].pop()
+            answer.append(value[0])
+            cnt += 1
+    
+    return answer
