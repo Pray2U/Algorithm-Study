fdfff1a33bd091ed3d8402965d66bce569de3647
chaeha617 <rlacogk6577@daum.net>
2023-03-20 09:32:59 +0900

    230313 순위 검색 답안 update

--- 230313 Programmers Lv.2 순위검색/230313_RankSearch_김채하.py
+++ 230313 Programmers Lv.2 순위검색/230313_RankSearch_김채하.py
@@ -1,20 +1,29 @@
 from itertools import product
+import bisect
+
 
 def solution(info, query):
-    items = [['cpp',"java","python"], ["backend","frontend"], ["junior","senior"],["chicken","pizza"]]
+    items = [['cpp', "java", "python"], ["backend", "frontend"], ["junior", "senior"], ["chicken", "pizza"]]
     val = list(product(*items))
+    val_dic = {''.join(v[:4]): [] for v in val}
+
     answer = []
-    val_dic = {''.join(v[:4]):[] for v in val}
+
     for inf in info:
         value = inf.split(" ")
         key = ''.join(value[:4])
         score = int(value[4])
         val_dic[key] += [score]
+
+    for cnt in val_dic.keys():
+        val_dic[cnt] = sorted(val_dic[cnt])
+
     for con in query:
         corect = 0
-        con = con.replace(" and "," ")
-        li =con.split()
+        con = con.replace(" and ", " ")
+        li = con.split()
         conli = []
+
         for ind, condition in enumerate(li):
             if ind == 4:
                 if condition == '-':
@@ -23,16 +32,12 @@ def solution(info, query):
                     conli.append([int(condition)])
             elif condition == '-':
                 conli.append(items[ind])
-            else :
+            else:
                 conli.append([condition])
+
         for condi in list(product(*conli)):
-            print(condi)
             score = condi[4]
             condi = ''.join(condi[:4])
-            for sco in val_dic[condi]:
-                if sco >= score:
-                    corect +=1
+            corect += len(val_dic[condi]) - bisect.bisect_left(val_dic[condi], score)
         answer += [corect]
     return answer
-
-print(solution(["java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50"],["java and backend and junior and pizza 100", "python and frontend and senior and chicken 200", "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100", "- and - and - and - 150"]))
