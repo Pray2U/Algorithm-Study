5fe088f6d9aac02e8272ff47abe0e3fce0cbf264
Choi JaeHoon <jhchoi09@naver.com>
2023-03-30 02:34:47 +0900

    Edit 문자열나누기

--- 230313 Programmers Lv.1 문자열나누기/문자열나누기_최재훈.py
+++ 230313 Programmers Lv.1 문자열나누기/문자열나누기_최재훈.py
@@ -1,7 +1,7 @@
 # Solution 01
 
 def solution(s):
-    answer, bucket = 0, [0, 0] # bucket[0] = 첫글자의 개수, bucket[1] = 첫글자와 다른 나머지 글자의 개수
+    answer, bucket = 1, [0, 0] # bucket[0] = 첫글자의 개수, bucket[1] = 첫글자와 다른 나머지 글자의 개수
     idx = 0
     for index, elem in enumerate(s):
         if elem == s[idx]:
@@ -13,14 +13,14 @@ def solution(s):
             answer += 1
             idx = index +1
             bucket[0], bucket[1] = 0, 0
-        else:
-            if index+1 >= len(s):
-                answer += 1
-                #print(f'{bucket} {answer} {index} {elem}')
-                break
-        #print(f'{bucket} {answer} {index} {elem}')
+        # else:
+        #     if index+1 >= len(s):
+        #         answer += 1
+        #         #print(f'{bucket} {answer} {index} {elem}')
+        #         break
+        # print(f'{bucket} {answer} {index} {elem}')
     return answer
 
-print(solution("banana")) #3
+# print(solution("banana")) #3
 print(solution("abracadabra")) #6
-print(solution("aaabbaccccabba")) #3
+# print(solution("aaabbaccccabba")) #3
