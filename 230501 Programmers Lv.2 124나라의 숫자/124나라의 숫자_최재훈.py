a07ac7f89c140c07fe8a10fa1101c4831b5d64ab
Choi JaeHoon <jhchoi09@naver.com>
2023-05-15 20:56:37 +0900

    2nd solution for 124 나라의 숫자

--- 230501 Programmers Lv.2 124나라의 숫자/124나라의 숫자_최재훈.py
+++ 230501 Programmers Lv.2 124나라의 숫자/124나라의 숫자_최재훈.py
@@ -24,3 +24,15 @@ def solution(n):
     return int(''.join(map(str, answer[::-1])))
 
 print(solution(11)) #42
+
+# def solution(n):
+#     arr = [4,1,2]
+#     result = ""
+#     while n > 0:
+#         result += str(arr[n%3])
+#         if n%3 == 0:
+#             n = n//3 -1
+#         else:
+#             n = n//3
+
+#     return result[::-1]
